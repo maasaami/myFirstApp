b501bf9bf7a48230040f79d04034fa223448a8bd
maasaami <104308937+maasaami@users.noreply.github.com>
2022-05-03 13:58:50 +0900

    update

--- src/components/catsTable.vue
+++ src/components/catsTable.vue
@@ -51,68 +51,100 @@ function filterAnimal(animals) {
 }
 const filteredCats = computed(() => filterAnimal(namesOfCats.value));
 const filteredDogs = computed(() => filterAnimal(namesOfDogs.value));
+
+// ダウンロード用
+function downloadCSV(list) {
+  let header = Object.keys(list[0]).join(",") + "\n";
+
+  let body = list.map(function (d) {
+    return Object.keys(d).map(function (key) {
+      return d[key];
+    }).join(',');
+  }).join("\n");
+
+  // const csv = header + body
+
+  const anchor = document.createElement('a');
+  anchor.href = 'data:text/csv;charset=utf-8,' + encodeURIComponent(header + body);
+  anchor.target = '_blank';
+  anchor.download = 'Animal List.csv';
+  anchor.click();
+
+};
 </script>
 
 <template>
 
-  <!-- 動物追加 -->
-  <div>
-    <input v-model="pushInputNameEn" placeholder="名前(英語)">
-    <input v-model="pushInputNameJa" placeholder="名前(日本語)">
-    <input type="number" v-model="pushInputAge" placeholder="年齢">
-    <button @click="pushAnimal(namesOfCats)">ねこ追加！</button>
-    <button @click="pushAnimal(namesOfDogs)">いぬ追加！</button>
-    <br>
-  </div>
+  <div class="container">
+    <!-- 動物追加 -->
+    <div>
+      <input v-model="pushInputNameEn" placeholder="名前(英語)">
+      <input v-model="pushInputNameJa" placeholder="名前(日本語)">
+      <input type="number" v-model="pushInputAge" placeholder="年齢">
+      <button @click="pushAnimal(namesOfCats)">ねこ追加！</button>
+      <button @click="pushAnimal(namesOfDogs)">いぬ追加！</button>
+      <br>
+    </div>
 
-  <!-- 検索フォーム -->
-  <div>
-    <input v-model="searchInput" placeholder="検索" class="searchInput">
-    <label><input type="checkbox" v-model="exactMatch">完全一致</label>
-    <h3><a href="#catList">猫該当:{{ filteredCats.length }}</a> <a href="#dogList">犬該当:{{ filteredDogs.length }}</a></h3>
-  </div>
+    <!-- 検索フォーム -->
+    <div>
+      <input v-model="searchInput" placeholder="検索" class="searchInput">
+      <label><input type="checkbox" v-model="exactMatch">完全一致</label>
+      <h3><a href="#catList">猫該当:{{ filteredCats.length }}</a> <a href="#dogList">犬該当:{{ filteredDogs.length }}</a></h3>
+    </div>
 
-  <!-- 猫のリスト -->
-  <div v-if="filteredCats.length" id="catList">
-    <h3>ねこ</h3>
-    <table>
-      <th>名前 (英語)</th>
-      <th>名前 (日本語)</th>
-      <th>年齢</th>
-      <tr v-for="cat in filteredCats">
-        <td>{{ cat.nameEn }}</td>
-        <td>{{ cat.nameJa }}</td>
-        <td>{{ cat.age }}</td>
-        <td style="text-align: center;">
-          <button @click="deletCat(cat)">削除</button>
-          <!-- <button @click="editCat(cat)">編集</button> -->
-        </td>
-      </tr>
-    </table>
-  </div>
+    <!-- 猫のリスト -->
+    <div v-if="filteredCats.length" id="catList">
+      <h2>ねこ</h2>
+      <div style="text-align: right;">
+        <button @click="downloadCSV(filteredCats)">CSV Download ↓</button>
+      </div>
+      <table>
+        <th>名前 (英語)</th>
+        <th>名前 (日本語)</th>
+        <th>年齢</th>
+        <tr v-for="cat in filteredCats">
+          <td>{{ cat.nameEn }}</td>
+          <td>{{ cat.nameJa }}</td>
+          <td>{{ cat.age }}</td>
+          <td style="text-align: center;">
+            <button @click="deletCat(cat)">削除</button>
+            <!-- <button @click="editCat(cat)">編集</button> -->
+          </td>
+        </tr>
+      </table>
+    </div>
 
-  <!-- 犬のリスト -->
-  <div v-if="filteredDogs.length" id="dogList">
-    <h3>いぬ</h3>
-    <table>
-      <th>名前 (英語)</th>
-      <th>名前 (日本語)</th>
-      <th>年齢</th>
-      <tr v-for="dog in filteredDogs">
-        <td>{{ dog.nameEn }}</td>
-        <td>{{ dog.nameJa }}</td>
-        <td>{{ dog.age }}</td>
-        <td style="text-align: center;">
-          <button @click="deletDog(dog)">削除</button>
-          <!-- <button @click="editCat(cat)">編集</button> -->
-        </td>
-      </tr>
-    </table>
+    <!-- 犬のリスト -->
+    <div v-if="filteredDogs.length" id="dogList">
+      <h2>いぬ</h2>
+      <div style="text-align: right;">
+        <button @click="downloadCSV(filteredDogs)">CSV Download ↓</button>
+      </div>
+      <table>
+        <th>名前 (英語)</th>
+        <th>名前 (日本語)</th>
+        <th>年齢</th>
+        <tr v-for="dog in filteredDogs">
+          <td>{{ dog.nameEn }}</td>
+          <td>{{ dog.nameJa }}</td>
+          <td>{{ dog.age }}</td>
+          <td style="text-align: center;">
+            <button @click="deletDog(dog)">削除</button>
+            <!-- <button @click="editCat(cat)">編集</button> -->
+          </td>
+        </tr>
+      </table>
+    </div>
   </div>
-
 </template>
 
 <style scoped>
+.container {
+  width: 80%;
+  margin: auto;
+}
+
 a {
   margin: 10px;
   text-decoration: none;
@@ -120,10 +152,9 @@ a {
 
 table {
   margin: auto;
-  margin-top: 10px;
   padding: 10px;
   border-collapse: collapse;
-  width: 60%;
+  width: 100%;
 }
 
 td {
@@ -134,10 +165,6 @@ td {
 
 input {
   margin: 5px;
-}
-
-.noResult {
-  text-decoration: underline;
 }
 
 .searchInput {
