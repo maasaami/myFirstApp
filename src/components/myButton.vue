a5a0074d565751666018300416cbd6d3114da82d
maasaami <104308937+maasaami@users.noreply.github.com>
2022-05-03 10:36:26 +0900

    update

--- src/components/myButton.vue
+++ src/components/myButton.vue
@@ -4,16 +4,16 @@ const countOfClick = ref('')
 </script>
 
 <template>
-<div id="myButton">
-  <button @click="countOfClick++">clicked : {{countOfClick}} times</button>
-  <p v-if="countOfClick">You've clicked {{countOfClick}} times!</p>
-</div>
+	<div id="myButton">
+		<button @click="countOfClick++">clicked : {{ countOfClick }} times</button>
+		<p v-if="countOfClick">You've clicked {{ countOfClick }} times!</p>
+	</div>
 </template>
 
 <style scoped>
 #myButton {
-		color: blue;
-		text-align: center;
-		padding: 10px;
-} 
+	color: blue;
+	text-align: center;
+	padding: 10px;
+}
 </style>
